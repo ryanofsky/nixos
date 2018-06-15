2c14e6e76e088edf9271caa03974696a3b16adf1
Russell Yanofsky <russ@yanofsky.org>
2018-06-15 15:02:56 +0000

    nix install config

--- configuration.nix
+++ configuration.nix
@@ -17,9 +17,9 @@
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "vdud"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Select internationalisation properties.
